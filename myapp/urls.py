564d29acec72e2fd15c38c71d36d02a74bdce454
Sarai Marte <smartepujols@gmail.com>
2023-10-21 00:22:58 -0400

    102123 statements must be seperated

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.home, name = "home"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
